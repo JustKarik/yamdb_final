bf0a56789ab242db012c796487fa75c6df8234c0
Даниил Лукащук <D.Lukashchuk@b.ru>
2023-03-28 11:16:21 +0300

    docker workflow

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -63,14 +63,12 @@ WSGI_APPLICATION = 'api_yamdb.wsgi.application'
 
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv('DB_NAME'),
-        'USER': os.getenv('POSTGRES_USER'),
-        'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
-        'HOST': os.getenv('DB_HOST'),
-        'PORT': os.getenv('DB_PORT')
-    }
+        'ENGINE': os.getenv('DB_ENGINE', default='django.db.backends.postgresql'),
+        'NAME': os.getenv('DB_NAME', default='data'),
+        'USER': os.getenv('POSTGRES_USER', default='author'),
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD', default='password'),
+        'HOST': os.getenv('DB_HOST', default='DB'),
+        'PORT': os.getenv('DB_PORT', default='0000'),
 }
 
 AUTH_PASSWORD_VALIDATORS = [
